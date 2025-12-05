19b0a1232e552b11d920602eaaec66a7a5466871
ShawaizB2004 <shawaizbhatti1@gmail.com>
2025-12-05 19:11:35 +1100

    Project complete

---
+++ SQL_Project_1_Files/5_optimal_skills.sql
@@ -0,0 +1,87 @@
+/*
+    Question: What are the most optimal skills to learn? There are the highest paid and in the highest demand.
+    - Identify skills in high demand and associated with high average salaries for Data Analyst roles.
+    - Concentrates on remote positions with specified salaries.
+    - Why? Targets skills that offer job security (high-demand) and financial benefits (high-salary),
+    offering insights for career development in Data Analytics.
+*/
+
+WITH skills_demand AS (
+    SELECT 
+        skills_dim.skill_id AS skill_id,
+        skills_dim.skills AS skills,
+        COUNT(job_postings_fact.job_id) AS demand_count
+    FROM
+        job_postings_fact
+    INNER JOIN 
+        skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN 
+        skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_postings_fact.job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere' AND
+        job_postings_fact.salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_dim.skill_id
+    ORDER BY
+        demand_count DESC
+), average_salary AS (
+    SELECT 
+        skills_dim.skill_id AS skill_id,
+        skills_dim.skills AS skills,
+        ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+    FROM
+        job_postings_fact
+    INNER JOIN 
+        skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN 
+        skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_postings_fact.job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere' AND
+        job_postings_fact.salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_dim.skill_id
+    ORDER BY
+        avg_salary DESC
+)
+
+SELECT 
+    skills_demand.skill_id,
+    skills_demand.skills,
+    average_salary.avg_salary,
+    skills_demand.demand_count
+FROM 
+    skills_demand
+INNER JOIN 
+    average_salary ON skills_demand.skill_id = average_salary.skill_id
+WHERE
+    demand_count > 10
+ORDER BY
+    demand_count DESC,
+    avg_salary DESC
+LIMIT 25;
+
+-- Shorter Query
+
+SELECT
+    skills_dim.skill_id AS skill_id,
+    skills_dim.skills AS skills,
+    ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary,
+    COUNT(job_postings_fact.job_id) AS demand_count
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_postings_fact.job_title_short = 'Data Analyst' AND
+    job_postings_fact.job_location = 'Anywhere' AND
+    job_postings_fact.salary_year_avg IS NOT NULL
+GROUP BY   
+    skills_dim.skill_id,
+    skills_dim.skills
+HAVING
+    COUNT(job_postings_fact.job_id) > 10
+ORDER BY
+    demand_count DESC,
+    avg_salary DESC;
